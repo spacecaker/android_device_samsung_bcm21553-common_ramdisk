47370ade7f452189bd4543636c30a48fbdcb011f
bieltv3 <bieltv.3@gmail.com>
2013-08-03 10:07:25 +0200

    Finally boot ext4 system, compatible with rfs, WIFI and BT working on CM7 - not on stock, bootanimation has been fixed, and smoe typo

--- sbin/bootscript.sh
+++ sbin/bootscript.sh
@@ -1,11 +1,10 @@
 #!/sbin/sh
 
-
 # mount
 	/sbin/busybox mount -o remount,rw / /;
 	/sbin/busybox mount -o remount,rw /dev/block/stl9 /system;
-# modules
 
+# modules
 /sbin/insmod /system/lib/modules/bcm_headsetsw.ko
 /sbin/insmod /system/lib/modules/brcm_headsetsw.ko
 /sbin/insmod /system/lib/modules/memalloc.ko
@@ -21,4 +20,5 @@
 /sbin/busybox chmod 755 /sbin/busybox
 
 setprop boot.ready 1
+
 exit 1
