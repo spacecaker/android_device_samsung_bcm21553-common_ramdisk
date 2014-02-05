dff5f8ec68aff0d65932263c6c46e860c981f7b5
bieltv3 <bieltv.3@gmail.com>
2014-02-05 23:36:48 +0100

    Small fix at bootscript

--- sbin/bootscript.sh
+++ sbin/bootscript.sh
@@ -1,20 +1,17 @@
 #!/sbin/sh
 
 # mount
-	/sbin/busybox mount -o remount,rw / /;
-	/sbin/busybox mount -o remount,rw /dev/block/stl9 /system;
+/sbin/busybox mount -o remount,rw / /;
+/sbin/busybox mount -o remount,rw /dev/block/stl9 /system;
 
 # modules
 /sbin/insmod /system/lib/modules/bcm_headsetsw.ko
-/sbin/insmod /system/lib/modules/brcm_headsetsw.ko
 /sbin/insmod /system/lib/modules/memalloc.ko
 /sbin/insmod /system/lib/modules/hx170dec.ko
 /sbin/insmod /system/lib/modules/h6270enc.ko
 /sbin/insmod /system/lib/modules/gememalloc.ko    
 /sbin/insmod /system/lib/modules/ge_drv.ko   
 /sbin/insmod /system/lib/modules/brcm_switch.ko
-/sbin/insmod /system/lib/modules/bcm_switch.ko
-
 
 /sbin/busybox chmod 755 /sbin
 /sbin/busybox chmod 755 /sbin/busybox
